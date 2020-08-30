4549e4963eed543ec0c07e909440a9e3d879ef7a
Jansen Price <sumpygump@gmail.com>
2020-08-30 01:20:45 -0500

    Add debug mode to exception handler to include more details and stack trace when handling exception

--- lib/Qi/Console/ExceptionHandler.php
+++ lib/Qi/Console/ExceptionHandler.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Exception Handler (console version) 
+ * Exception Handler (console version)
  *
  * @package Qi
  * @subpackage Console
@@ -8,7 +8,7 @@
 
 /**
  * Console Exception Handler class
- * 
+ *
  * @package Qi
  * @subpackage Console
  * @author Jansen Price <jansen.price@gmail.com>
@@ -16,36 +16,40 @@
  */
 class Qi_Console_ExceptionHandler
 {
+    protected $isDebug = false;
+
     /**
      * Constructor
-     * 
+     *
      * @param Qi_Console_Terminal $terminal Terminal object
      * @param bool $bindHandlers Whether to immediately bind handlers
      * @return void
      */
-    public function __construct($terminal, $bindHandlers = false)
+    public function __construct($terminal, $bindHandlers = false, $isDebug = false)
     {
         $this->_terminal = $terminal;
 
         if ($bindHandlers === true) {
-            $this->bindHandlers();
+            $this->bindHandlers($isDebug);
         }
     }
 
     /**
      * Bind error handlers
-     * 
+     *
      * @return void
      */
-    public function bindHandlers()
+    public function bindHandlers($isDebug = false)
     {
+        $this->isDebug = (bool) $isDebug;
+
         set_exception_handler(array($this, 'handleException'));
         set_error_handler(array($this, 'handleError'));
     }
 
     /**
      * Handle error
-     * 
+     *
      * @return void
      */
     public function handleError()
@@ -62,16 +66,29 @@ class Qi_Console_ExceptionHandler
 
     /**
      * Handle exception
-     * 
+     *
      * @param Exception $exception Exception object
      * @return void
      */
     public function handleException(\Throwable $exception)
     {
+        $message = sprintf("%s: %s", get_class($exception), $exception->getMessage());
+
+        if ($this->isDebug) {
+            $message = sprintf(
+                "%s: %s in %s on line %s",
+                get_class($exception), $exception->getMessage(), $exception->getFile(), $exception->getLine()
+            );
+        }
+
         echo "\n";
-        $this->_terminal->pretty_message($exception->getMessage(), 7, 1);
+        $this->_terminal->pretty_message($message, 7, 1);
         echo "\n";
-        
+
+        if ($this->isDebug) {
+            printf("%s\n", $exception->getTraceAsString());
+        }
+
         exit(1);
     }
 
